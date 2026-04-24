50864cb1df4a692b17ccdf9c461abc8470d9076a
siva-aicoder <p59611423@gmail.com>
2026-04-24 08:25:54 +0000

    AI: automated code changes for feature/KAN-134

--- src/main/java/com/store/delivery/service/OrderDeliveryService.java
+++ src/main/java/com/store/delivery/service/OrderDeliveryService.java
@@ -4,6 +4,7 @@ import com.store.delivery.constant.DeliveryServiceConstants;
 import com.store.delivery.entity.OrderDetails;
 import org.springframework.stereotype.Service;
 import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -11,43 +12,75 @@ import java.util.List;
 public class OrderDeliveryService {
 
     public List<OrderDetails> deliveryStoreMapping(List<OrderDetails> orderDetails){
+        List<OrderDetails> deliveryMappedOrders = new ArrayList<>();
 
-        orderDetails.forEach(order ->{
-            LocalTime orderTime = LocalTime.parse(
-                    order.getOrderTimestamp(),
-                    DeliveryServiceConstants.TIME_FORMATTER
-            );
+        orderDetails.forEach(order -> {
+            OrderDetails mappedOrder = copyOrderDetails(order);
 
-            boolean isWithinHours = orderTime.isAfter(DeliveryServiceConstants.OPENING_TIME) &&
-                    orderTime.isBefore(DeliveryServiceConstants.CLOSING_TIME);
-
-            if(isWithinHours){
-                 if(order.getDeliveryCity().equals("Manchester")){
-                    order.setDeliveryStoreName(DeliveryServiceConstants.MANCHESTER_STORE_NAME);
-                }else if(order.getDeliveryCity().equals("London")){
-                    order.setDeliveryStoreName(DeliveryServiceConstants.LONDON_STORE_NAME);
-                }else if(order.getDeliveryCity().equals("Cambridge")){
-                    order.setDeliveryStoreName(DeliveryServiceConstants.CAMBRIDGE_STORE_NAME);
-                }
+            if(isWithinMappingHours(order)){
+                mappedOrder.setDeliveryStoreName(getMappedStoreName(order.getDeliveryCity()));
             }else{
-                order.setDeliveryStoreName(DeliveryServiceConstants.UNSCHEDULED_STORE_NAME);
+                mappedOrder.setDeliveryStoreName(DeliveryServiceConstants.UNSCHEDULED_STORE_NAME);
             }
 
-
+            deliveryMappedOrders.add(mappedOrder);
         });
 
-        return orderDetails;
+        return deliveryMappedOrders;
 
     }
 
     public List<OrderDetails> deliveryUnscheduledStoreMapping(List<OrderDetails> orderDetails){
+        List<OrderDetails> deliveryMappedOrders = new ArrayList<>();
+
         orderDetails.forEach(order ->{
-            if(order.getDeliveryStoreName().equals(DeliveryServiceConstants.UNSCHEDULED_STORE_NAME)){
-                order.setDeliveryStoreName(DeliveryServiceConstants.MANCHESTER_STORE_NAME);
+            OrderDetails mappedOrder = copyOrderDetails(order);
+
+            if(isWithinMappingHours(order)){
+                mappedOrder.setDeliveryStoreName(getMappedStoreName(order.getDeliveryCity()));
             }
+
+            deliveryMappedOrders.add(mappedOrder);
         });
 
-        return orderDetails;
+        return deliveryMappedOrders;
+    }
+
+    private boolean isWithinMappingHours(OrderDetails order) {
+        LocalTime orderTime = LocalTime.parse(
+                order.getOrderTimestamp(),
+                DeliveryServiceConstants.TIME_FORMATTER
+        );
+
+        return orderTime.isAfter(DeliveryServiceConstants.OPENING_TIME) &&
+                orderTime.isBefore(DeliveryServiceConstants.CLOSING_TIME);
+    }
+
+    private String getMappedStoreName(String deliveryCity) {
+        if("Manchester".equals(deliveryCity)){
+            return DeliveryServiceConstants.MANCHESTER_STORE_NAME;
+        }else if("London".equals(deliveryCity)){
+            return DeliveryServiceConstants.LONDON_STORE_NAME;
+        }else if("Cambridge".equals(deliveryCity)){
+            return DeliveryServiceConstants.CAMBRIDGE_STORE_NAME;
+        }
+
+        return DeliveryServiceConstants.UNSCHEDULED_STORE_NAME;
+    }
+
+    private OrderDetails copyOrderDetails(OrderDetails order) {
+        return OrderDetails.builder()
+                .orderId(order.getOrderId())
+                .productName(order.getProductName())
+                .productCategory(order.getProductCategory())
+                .productWeight(order.getProductWeight())
+                .deliveryCity(order.getDeliveryCity())
+                .orderTimestamp(order.getOrderTimestamp())
+                .deliveryPostalCode(order.getDeliveryPostalCode())
+                .tripDistance(order.getTripDistance())
+                .vehicleType(order.getVehicleType())
+                .deliveryStoreName(order.getDeliveryStoreName())
+                .build();
     }
 
 }
