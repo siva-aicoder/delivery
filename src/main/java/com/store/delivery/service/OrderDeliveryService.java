f7e84e77bab2289f73e703d88a0bfffc4b5199dd
siva-aicoder <p59611423@gmail.com>
2026-05-15 05:50:22 +0000

    AI: automated code changes for feature/KAN-144

--- src/main/java/com/store/delivery/service/OrderDeliveryService.java
+++ src/main/java/com/store/delivery/service/OrderDeliveryService.java
@@ -22,13 +22,7 @@ public class OrderDeliveryService {
                     orderTime.isBefore(DeliveryServiceConstants.CLOSING_TIME);
 
             if(isWithinHours){
-                 if(order.getDeliveryCity().equals("Manchester")){
-                    order.setDeliveryStoreName(DeliveryServiceConstants.MANCHESTER_STORE_NAME);
-                }else if(order.getDeliveryCity().equals("London")){
-                    order.setDeliveryStoreName(DeliveryServiceConstants.LONDON_STORE_NAME);
-                }else if(order.getDeliveryCity().equals("Cambridge")){
-                    order.setDeliveryStoreName(DeliveryServiceConstants.CAMBRIDGE_STORE_NAME);
-                }
+                order.setDeliveryStoreName(getStoreNameByDeliveryCity(order.getDeliveryCity()));
             }else{
                 order.setDeliveryStoreName(DeliveryServiceConstants.UNSCHEDULED_STORE_NAME);
             }
@@ -43,11 +37,23 @@ public class OrderDeliveryService {
     public List<OrderDetails> deliveryUnscheduledStoreMapping(List<OrderDetails> orderDetails){
         orderDetails.forEach(order ->{
             if(order.getDeliveryStoreName().equals(DeliveryServiceConstants.UNSCHEDULED_STORE_NAME)){
-                order.setDeliveryStoreName(DeliveryServiceConstants.MANCHESTER_STORE_NAME);
+                order.setDeliveryStoreName(getStoreNameByDeliveryCity(order.getDeliveryCity()));
             }
         });
 
         return orderDetails;
+    }
+
+    private String getStoreNameByDeliveryCity(String deliveryCity) {
+        if ("Manchester".equals(deliveryCity)) {
+            return DeliveryServiceConstants.MANCHESTER_STORE_NAME;
+        } else if ("London".equals(deliveryCity)) {
+            return DeliveryServiceConstants.LONDON_STORE_NAME;
+        } else if ("Cambridge".equals(deliveryCity)) {
+            return DeliveryServiceConstants.CAMBRIDGE_STORE_NAME;
+        }
+
+        return DeliveryServiceConstants.UNSCHEDULED_STORE_NAME;
     }
 
 }
